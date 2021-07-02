122f6cd21cc02a36e790c5d22232e540bd3d8647
Tiago Sampaiogit config --global user.name Tiago <Teagol10@gmail.com>
2021-07-02 15:43:46 -0300

    Delete implemetation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner tc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
@@ -43,8 +46,13 @@ public class Program {
 		sellerDao.update(seller);
 		System.out.println("Update completed");
 		
+		System.out.println("\n=== TEST 6: seller delete ====");
+		System.out.println("Enter id for delete test: ");
+		int id  = tc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("DELETE completed");
 		
-		
+		tc.close();
 	}
 
 }
